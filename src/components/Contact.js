465230ca61d4ba1a4fed001694e12bcfc659a0bf
SamHodg1993 <samhodgkinson9192@gmail.com>
2022-12-22 21:02:22 +0000

    Completed iPad screen functionality for all components except navbar#

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -52,7 +52,7 @@ function Contact() {
             </p>
           </ul>
         </div>
-        <div className='contact-row contact-row-1'>
+        <div className='contact-row'>
           <div className='google-map'>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2221.0677793825294!2d-3.0380219709357155!3d56.17322116872863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48864cd19e5b26d7%3A0x20a160f4f7fb2ae7!2sRobert%20Summers%20Transport%20Ltd!5e0!3m2!1sen!2suk!4v1670162472822!5m2!1sen!2suk" 
               width={screenWidth-20} 
