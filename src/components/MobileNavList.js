8663794b69b13d012039578ab78c70cb1aa42b79
SamHodg1993 <samhodgkinson9192@gmail.com>
2023-01-03 09:17:26 +0000

    Updated build folder to try out new mobile nav tactic

--- src/components/MobileNavList.js
+++ src/components/MobileNavList.js
@@ -8,12 +8,17 @@ const Navbar = () => {
 
     const {setShowMobileNavList} = useContext(AppContext);
 
+    const handleNavClick = () => {
+        setShowMobileNavList(false);
+        <Link to={'/about'}/>
+    }
+
     return(
         <nav className='mobile-nav-list-container'>
             <ul className='mobile-nav-list-container'>
               <Link to='/' className='mobile-nav-list-item' onClick={() => setShowMobileNavList(false)}>Home</Link>
               <Link to='/contact' className='mobile-nav-list-item' onClick={() => setShowMobileNavList(false)}>Contact</Link>
-              <Link to='/about' className='mobile-nav-list-item' onClick={() => setShowMobileNavList(false)}>About</Link>
+              <Link to='/about' className='mobile-nav-list-item' onClick={() => handleNavClick()}>About</Link>
               <Link to='/services' className='mobile-nav-list-item' onClick={() => setShowMobileNavList(false)}>Services</Link>
               <Link to='/gallary' className='mobile-nav-list-item' onClick={() => setShowMobileNavList(false)}>Gallary</Link>
             </ul>
