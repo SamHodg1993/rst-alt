1cc908e1389e1750e2b0ee92a5741fc6845055c1
SamHodg1993 <samhodgkinson9192@gmail.com>
2023-01-08 16:18:01 +0000

    Added the react-burger-menu package

--- src/components/MobileNavbar.js
+++ src/components/MobileNavbar.js
@@ -1,6 +1,7 @@
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { IconContext } from 'react-icons';
 import { useContext } from 'react';
+import { slide as Menu } from 'react-burger-menu';
 
 import logo from '../images/logo-removebg.png';
 
@@ -12,11 +13,18 @@ export default function MobileNavbar () {
   const {showMobileList, setShowMobileList} = useContext(AppContext);
 
   return(
-    <IconContext.Provider value={{color:'rgba(32,33,36,0.7)', size:'8rem'}}>
+    <div>
+      Hi there
+    </div>
+  )
+}
+
+
+// -------------------------- Removed Code ---------------------------- 
+
+{/* <IconContext.Provider value={{color:'rgba(32,33,36,0.7)', size:'8rem'}}>
       <div className='mobile-nav-cont'>
         <img src={logo} alt="RST logo" className="logo" />
           <GiHamburgerMenu className='hamburger-icon' onClick={() => {showMobileList ? setShowMobileList(false) : setShowMobileList(true)}} />
       </div>
-      </IconContext.Provider>
-  )
-}
+      </IconContext.Provider> */}
